75f17902d8d757abff02828197b82dd1c86e2482
19104146 <19104146@usc.edu.ph>
2024-05-13 03:17:58 +0800

    feat:add header padding

--- app/(tabs)/more/index.tsx
+++ app/(tabs)/more/index.tsx
@@ -1,16 +1,19 @@
+import { useHeaderHeight } from "@react-navigation/elements";
 import { LinearGradient } from "expo-linear-gradient";
 import { StyleSheet, View } from "react-native";
 
 export default function MoreScreen() {
+  const headerHeight = useHeaderHeight();
+
   return (
-    <View style={styles.container}>
+    <View style={StyleSheet.compose(styles.container, { paddingTop: headerHeight })}>
       <LinearGradient
         dither={false}
         colors={["#D99536", "#B77E2E", "#936525", "#69481A", "#452F11", "#191106", "#0D0903", "#060402", "#000000"]}
         locations={[0, 0.03, 0.06, 0.11, 0.16, 0.27, 0.67, 0.86, 1]}
         start={{ x: 0, y: 0 }}
         end={{ x: 1, y: 0.5 }}
-        style={styles.background}
+        style={StyleSheet.absoluteFill}
       />
     </View>
   );
@@ -21,13 +24,5 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "flex-start",
-  },
-  background: {
-    position: "absolute",
-    left: 0,
-    right: 0,
-    top: 0,
-    bottom: 0,
-    flex: 1,
   },
 });
