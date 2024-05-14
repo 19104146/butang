8cb98ef952d2571879c6c3b81440aa97be6568f2
Kau4r <22102606@usc.edu.ph>
2024-05-14 19:43:16 +0800

    feat: add more at more

--- app/(tabs)/more/index.tsx
+++ app/(tabs)/more/index.tsx
@@ -1,12 +1,11 @@
-import { useHeaderHeight } from "@react-navigation/elements";
+import { Image } from "expo-image";
 import { LinearGradient } from "expo-linear-gradient";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function MoreScreen() {
-  const headerHeight = useHeaderHeight();
-
   return (
-    <View style={StyleSheet.compose(styles.container, { paddingTop: headerHeight + 20 })}>
+    <SafeAreaView style={styles.container}>
       <LinearGradient
         dither={false}
         colors={["#D99536", "#B77E2E", "#936525", "#69481A", "#452F11", "#191106", "#0D0903", "#060402", "#000000"]}
@@ -15,7 +14,19 @@ export default function MoreScreen() {
         end={{ x: 1, y: 0.5 }}
         style={StyleSheet.absoluteFill}
       />
-    </View>
+      <View style={styles.card}>
+        <LinearGradient
+          dither={false}
+          colors={["#925D11", "#6F4A16", "#573E1A", "#45351D", "#2D2922", "#201E1B"]}
+          locations={[0, 0.13, 0.29, 0.41, 0.68, 1]}
+          start={{ x: 0, y: 0 }}
+          end={{ x: 0, y: 1 }}
+          style={StyleSheet.compose(StyleSheet.absoluteFill, { borderRadius: 20 })}
+        />
+        <Image style={styles.logo} source={require("../../assets/icon.png")} />
+        <Text style={styles.butang}>Butang</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -24,7 +35,24 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "flex-start",
+    flexWrap: "wrap",
     padding: 20,
-    gap: 20,
+    color: "#E8EAED",
+  },
+  card: {
+    width: "100%",
+    height: "30%",
+    borderRadius: 20,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  logo: {
+    width: "50%",
+    height: "50%",
+    alignSelf: "center",
+  },
+  butang: {
+    color: "#C88426",
+    fontSize: 30,
   },
 });
