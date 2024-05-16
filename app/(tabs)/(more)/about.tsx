6e5b4310bec001498548a987355da96095b9a988
Kau4r <22102606@usc.edu.ph>
2024-05-16 23:34:50 +0800

    feat: add backround

--- app/(tabs)/(more)/about.tsx
+++ app/(tabs)/(more)/about.tsx
@@ -1,21 +1,23 @@
+import { LinearGradient } from "expo-linear-gradient";
 import { StyleSheet, Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
 
-export default function AboutScreen() {
+export default function About() {
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>About</Text>
-    </SafeAreaView>
+    <View style={style.container}>
+      <LinearGradient
+        dither={false}
+        colors={["#D99536", "#B77E2E", "#936525", "#69481A", "#452F11", "#191106", "#0D0903", "#060402", "#000000"]}
+        locations={[0, 0.03, 0.06, 0.11, 0.16, 0.27, 0.67, 0.86, 1]}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 0.5 }}
+        style={StyleSheet.absoluteFill}
+      />
+    </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    flexWrap: "wrap",
-    padding: 20,
-    backgroundColor: "blue",
+const style= StyleSheet.create({
+  container:{
+    flex:1,
   },
 });
