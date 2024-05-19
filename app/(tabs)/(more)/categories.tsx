753112cdd5f260005c9c1a26b8bf3e5378f28a7f
Kau4r <22102606@usc.edu.ph>
2024-05-19 22:08:20 +0800

    feat: add dummy in categories

--- app/(tabs)/(more)/categories.tsx
+++ app/(tabs)/(more)/categories.tsx
@@ -1,15 +1,17 @@
+import { useHeaderHeight } from "@react-navigation/elements";
 import { LinearGradient } from "expo-linear-gradient";
 import { StyleSheet, Text, View } from "react-native";
 
 const dummyCategories = [
-  { label: "All", value: null },
   { label: "Medicine", value: "1" },
   { label: "Beverage", value: "2" },
 ];
 
 export default function Categories() {
+  const headerHeight = useHeaderHeight();
+
   return (
-    <View style={style.container}>
+    <View style={[style.container, { paddingTop: headerHeight }]}>
       <LinearGradient
         dither={false}
         colors={["#D99536", "#B77E2E", "#936525", "#69481A", "#452F11", "#191106", "#0D0903", "#060402", "#000000"]}
@@ -18,15 +20,35 @@ export default function Categories() {
         end={{ x: 1, y: 0.5 }}
         style={StyleSheet.absoluteFill}
       />
+      {dummyCategories.map((Categories) => (
+        
+        <View key={Categories.label} style={{ flexDirection: "row", justifyContent: "space-between",alignContent:"center",margin:10, padding:10,paddingHorizontal:20,borderColor:"#D0D0D020",borderWidth:1,borderRadius:15}}>
+          <LinearGradient
+          dither={false}
+          colors={["#925D11", "#6F4A16", "#573E1A", "#45351D", "#2D2922", "#201E1B"]}
+          locations={[0, 0.13, 0.29, 0.41, 0.68, 1]}
+          start={{ x: 1, y: 0 }}
+          end={{ x: 0, y: 0 }}
+          style={StyleSheet.compose(StyleSheet.absoluteFill, { borderRadius: 15 })}
+        />
+          <Text style={style.text}>{Categories.label}</Text>
+          <View style={{backgroundColor:"#6A655F" ,paddingHorizontal:10, alignSelf:"center",borderRadius:10}}>
+            <Text style={{fontSize:14,color:"#FFE9CB"}}>{Categories.value}</Text>
+          </View>
+        </View>
+      ))}
     </View>
-
-    
-
   );
 }
 
-const style= StyleSheet.create({
-  container:{
-    flex:1,
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: "700",
+    color: "#FFE9CB",
   },
 });
