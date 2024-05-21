dde9e5f3faa83646e1598f04e3bd9677067fcf5b
Prince Isaac Pantino <arispantino@gmail.com>
2024-05-21 20:57:13 +0800

    feat: nothing

--- components/ItemModal.tsx
+++ components/ItemModal.tsx
@@ -180,7 +180,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "rgba(0, 0, 0, 0.7)",
+    backgroundColor: "rgba(0, 0, 0, 0.6)",
   },
   modalView: {
     display: "flex",
