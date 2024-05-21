194612187b33681ee3dd0fb75ba717d3e81067f7
19104146 <19104146@usc.edu.ph>
2024-05-22 02:17:53 +0800

    feat: cleanup overview

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,10 @@
-import { MaterialIcons } from "@expo/vector-icons";
 import { useBottomTabBarHeight } from "@react-navigation/bottom-tabs";
 import { useHeaderHeight } from "@react-navigation/elements";
 import { LinearGradient } from "expo-linear-gradient";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, View } from "react-native";
 
 import LowStocks from "@/components/LowStocks";
+import { CategoryCard, ItemCard, RecentCard, SomethingCard } from "@/components/OverviewCards";
 import RecentActivity from "@/components/RecentActivity";
 
 export default function OverviewScreen() {
@@ -25,65 +25,14 @@ export default function OverviewScreen() {
       />
       <LowStocks />
       <RecentActivity />
-      <View style={{ flexDirection: "row", flexWrap: "wrap", height: "auto", flex: 1, gap: 20 }}>
-        <View style={{ flex: 1, gap: 15 }}>
-          <LinearGradient
-            dither={false}
-            colors={["#925D11", "#6F4A16", "#573E1A", "#45351D", "#2D2922", "#201E1B"]}
-            locations={[0, 0.13, 0.29, 0.41, 0.68, 1]}
-            start={{ x: 1, y: 0 }}
-            end={{ x: 0, y: 1 }}
-            style={[styles.lowStockView, { height: "60%", width: "100%" }]}
-          >
-            <View style={[styles.container, { gap: 10 }]}>
-              <Text style={[styles.headerText, { fontSize: 32 }]}>529</Text>
-              <Text style={{ color: "#8A8A8A" }}>Items</Text>
-            </View>
-          </LinearGradient>
-          <LinearGradient
-            dither={false}
-            colors={["#925D11", "#6F4A16", "#573E1A", "#45351D", "#2D2922", "#201E1B"]}
-            locations={[0, 0.13, 0.29, 0.41, 0.68, 1]}
-            start={{ x: 1, y: 0 }}
-            end={{ x: 0, y: 1 }}
-            style={[styles.lowStockView, { height: "40%", width: "100%" }]}
-          >
-            <View style={[styles.container, { gap: 0, paddingHorizontal: 0 }]}>
-              <Text style={[styles.headerText, { fontSize: 32 }]}>100</Text>
-              <Text style={{ color: "#8A8A8A" }}>Categories</Text>
-            </View>
-          </LinearGradient>
+      <View style={styles.innerContainer}>
+        <View style={styles.splitContainer}>
+          <ItemCard />
+          <CategoryCard />
         </View>
-        <View style={{ flex: 1, gap: 15 }}>
-          <LinearGradient
-            dither={false}
-            colors={["#925D11", "#6F4A16", "#573E1A", "#45351D", "#2D2922", "#201E1B"]}
-            locations={[0, 0.13, 0.29, 0.41, 0.68, 1]}
-            start={{ x: 1, y: 0 }}
-            end={{ x: 0, y: 1 }}
-            style={[styles.lowStockView, { height: "40%", width: "100%" }]}
-          >
-            <View style={[styles.container, { gap: 10 }]}>
-              <Text style={[styles.headerText, { fontSize: 32 }]}>420</Text>
-              <Text style={{ color: "#8A8A8A" }}>Recents</Text>
-            </View>
-          </LinearGradient>
-          <LinearGradient
-            dither={false}
-            colors={["#925D11", "#6F4A16", "#573E1A", "#45351D", "#2D2922", "#201E1B"]}
-            locations={[0, 0.13, 0.29, 0.41, 0.68, 1]}
-            start={{ x: 1, y: 0 }}
-            end={{ x: 0, y: 1 }}
-            style={[styles.lowStockView, { height: "60%", width: "100%" }]}
-          >
-            <View style={[styles.container, { gap: 10 }]}>
-              <Text style={[styles.headerText, { fontSize: 32 }]}>9</Text>
-              <View style={{ flexDirection: "row", alignItems: "center" }}>
-                <Text style={{ color: "#8A8A8A", alignSelf: "flex-start" }}>Medicine</Text>
-                <MaterialIcons name="keyboard-arrow-down" size={20} color="#8A8A8A" />
-              </View>
-            </View>
-          </LinearGradient>
+        <View style={styles.splitContainer}>
+          <RecentCard />
+          <SomethingCard />
         </View>
       </View>
     </SafeAreaView>
@@ -98,33 +47,15 @@ const styles = StyleSheet.create({
     padding: 20,
     gap: 20,
   },
-  headerText: {
-    fontSize: 20,
-    color: "#FFE9CB",
-    fontWeight: "bold",
-  },
-  contentText: {
-    color: "#FFE9CB",
-    fontSize: 16,
-  },
-  lowStockView: {
-    backgroundColor: "transparent",
-    paddingHorizontal: 25,
-    paddingVertical: 15,
-    borderRadius: 20,
-    width: "100%",
-    height: "27.5%",
-    justifyContent: "flex-start",
-  },
-  itemContainer: {
-    flex: 1,
-    paddingBottom: 10,
-    // backgroundColor: 'red',
-  },
-  viewHeader: {
+  innerContainer: {
     flexDirection: "row",
-    height: 30,
-    alignItems: "center",
-    gap: 10,
+    flexWrap: "wrap",
+    height: "auto",
+    flex: 1,
+    gap: 20,
+  },
+  splitContainer: {
+    flex: 1,
+    gap: 15,
   },
 });
